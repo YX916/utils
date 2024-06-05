338a688f77601bbe6fcb30f74a85f6ab2d5bf5ba
杨永鑫 <3271310698@qq.com>
2024-06-05 13:46:47 +0800

    push

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -5,24 +5,24 @@
  * @returns 格式化后的字符串
  */
 function format(date: Date, formatStr: string): string {
-    let str = formatStr.replace(/YYYY|MM|mm|DD|dd|HH|hh|mm|ss/g, function (match): string {
+    let str = formatStr.replace(/YYYY-|MM-|mm-|DD|dd|HH:|hh:|mm:|ss/g, function (match): string {
         switch (match) {
-            case 'YYYY':
-                return date.getFullYear().toString();
-            case 'MM':
-                return (date.getMonth() + 1).toString().padStart(2, '0');
-            case 'mm':
-                return (date.getMonth() + 1).toString();
+            case 'YYYY-':
+                return date.getFullYear().toString() + "-";
+            case 'MM-':
+                return (date.getMonth() + 1).toString().padStart(2, '0') + "-";
+            case 'mm-':
+                return (date.getMonth() + 1).toString() + "-";
             case 'DD':
                 return date.getDate().toString().padStart(2, '0');
             case 'dd':
                 return date.getDate().toString();
-            case 'HH':
-                return date.getHours().toString().padStart(2, '0');
-            case 'hh':
-                return date.getHours().toString();
-            case 'mm':
-                return date.getMinutes().toString().padStart(2, '0');
+            case 'HH:':
+                return date.getHours().toString().padStart(2, '0') + ":";
+            case 'hh:':
+                return date.getHours().toString() + ":";
+            case 'mm:':
+                return date.getMinutes().toString().padStart(2, '0') + ":";
             case 'ss':
                 return date.getSeconds().toString().padStart(2, '0');
         }
